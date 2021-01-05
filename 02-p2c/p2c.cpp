2597524c4957a181f332a6d8cbcd16ba7210da29
Victor M. Emanuel <victor.m.emanuel@gmail.com>
2021-01-05 12:54:08 -0500

    started p2c.cpp - power to choose text RPG.

---
+++ 02-p2c/p2c.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+
+// ****************************************************************************
+int main()
+{
+    cout << "Welcome: Power to Choose.\n";
+}
